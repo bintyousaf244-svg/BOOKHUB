03d85382c0ce161271a39ee1f71736c98e41e762
bintyousaf244-svg <bintyousaf244@gmail.com>
2026-05-12 22:43:48 +0500

    Update main.tsx

--- artifacts/bookstore/src/main.tsx
+++ artifacts/bookstore/src/main.tsx
@@ -2,7 +2,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-import { setBaseUrl } from "./lib/customFetch";
+import { setBaseUrl } from "../lib/api-client-react/src/custom-fetch";
 
 setBaseUrl("/api");
 
