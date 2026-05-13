97103c26a477d9d329e0ae34376f89a2c371d2e9
bintyousaf244-svg <bintyousaf244@gmail.com>
2026-05-13 12:46:30 +0500

    commit

--- artifacts/bookstore/src/main.tsx
+++ artifacts/bookstore/src/main.tsx
@@ -2,4 +2,8 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
+import { setBaseUrl } from "../../lib/api-client-react/src/custom-fetch";
+
+setBaseUrl("/api");
+
 createRoot(document.getElementById("root")!).render(<App />);
